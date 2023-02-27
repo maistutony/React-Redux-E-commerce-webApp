8f0a85c55b7fad791f822742f1a3e78691022017
Antony Maingi <maingitony101@gmail.com>
2023-02-27 18:44:49 +0300

    Inintial commit to my react-redux ecommerce web app

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NewArrivals from './NewArrivals'
+
+function Home() {
+  return (
+    <div>
+        <div>this is the home page</div>
+        <NewArrivals/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navbar, Nav} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="">Athi Fashions</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Link className="p-3" to="/">Home</Link>
+          <Link className="p-3" to="/products">Products</Link>
+          <Link className="p-3" to="/cart">Cart</Link>
+          <Link className="p-3" to="/login">LogIn</Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default NavBar;
